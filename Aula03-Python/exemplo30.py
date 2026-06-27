40ff9a7838a2cde34bb11dfab296db1b09c6f426
Sergio Gustavo Medina Pereira <medinasergiogustavo@gmail.com>
2026-06-27 10:53:39 -0300

    Add files via upload

---
+++ Aula03-Python/exemplo30.py
@@ -0,0 +1,42 @@
+"""
+PI = 3.14   # constante tipo float
+GRAVIDADE = 9.8   # constante tipo float
+
+num1 = 7.5 # variavel tipo float
+num2 = 4   # variavel tipo int
+ 
+print("Valor da constante PI =", PI)
+print("Valor da constante GRAVIDADE =", GRAVIDADE)
+
+num = input("Digite um numero =") # 5
+#num = int(input("Digite um numero =") )
+num_int = int(num) #5
+
+print ("Concateção de m =",num+num) # 55
+print ("Soma =",num_int+num_int) # 
+
+x, y, z = 10, 50, 80
+
+print("O valor da variavel x = " +str(x))
+print("O valor da variavel x = " , x)
+type(x)
+print("Tipo da variavel x = ",type(x))
+
+print("O valor da variavel y = " + str(y))
+print("O valor da variavel z = " , str(z))
+
+print("Soma das variaveis x+x = ", x+x)
+
+print("Resultado da soma x+(x+y) =", x+(y+y))
+
+print(x-x)
+print((x*x)-z)  # x= 10, z= 80
+"""
+num1= 6
+num2= 8
+media = (num1 + num2) /2   # num1=6 e num2=8, media= 10
+print("A media é = ", media)
+print(num1/5)  # x =10
+print(num1/6)
+print(num1//6)
+
