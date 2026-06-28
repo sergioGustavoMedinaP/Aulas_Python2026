07e87b0ebcf0c0ca73bd4c06e321c5ac08d989c8
Sergio Gustavo Medina Pereira <medinasergiogustavo@gmail.com>
2026-06-28 11:25:39 -0300

    Add files via upload

---
+++ Aula07-E_Dados-Python/Aula08-Python/exemplo80.py
@@ -0,0 +1,29 @@
+
+print("Exemplo de Utilização de listas, que são mutaveies");
+print("");
+listaCarros = ['Creta', 'Azera', 'HB20', 'Tucson','Santa Fe', 'IX35'];
+print("Carro que esta na posição 5 = ", listaCarros [5]);
+
+print("Lista os carros que estão entre a posição 0 até o 4 = ", listaCarros [0:4]);
+
+
+print("");
+listaPerguntas = ["Quem descobriu o Brazil?",
+                      "Em que ano?",
+                      "Qual o regime político do Brasil hoje?",
+                      "Qual o principal problema do Brasil hoje?",
+                      "Qual é a maior cidade brasileira?"]
+                       
+listaRespostas = ["Pedro Álvares Cabral",
+                      "1500",
+                      "Democracia",
+                      "Educação de qualidade",
+                      "São Paulo"]
+                       
+                                         
+print (listaPerguntas[0], " Foi " , listaRespostas[0] , "!")
+     
+print (listaRespostas)
+     
+print (listaPerguntas[3:5])
+print (listaRespostas[3:5])
---
+++ Aula07-E_Dados-Python/Aula08-Python/exemplo82.py
@@ -0,0 +1,33 @@
+#Sintaxe de uma tuplaPython
+exemploTupla = ("a", "e", "i", "o", "u")
+
+primeiro = exemploTupla[0]
+print(primeiro)
+
+segundo = exemploTupla[1]
+print(segundo)
+#Exemplo de tupla Estado
+tuplaEstado = ("Rio de Janeiro",
+                   "São Paulo",
+                   "Minas Gerais",
+                   "Bahia",
+                   "Amazonas")
+
+#Exemplo de tupla Capital                 
+tuplaCapital = ("Rio de Janeiro",
+                    "São Paulo",
+                    "Belo Horizonte",
+                    "Salvador",
+                    "Manaus")
+                       
+print("Lista da tuplas Estado")                                  
+print (tuplaEstado)
+
+print("Lista da tuplas Capital") 
+print (tuplaCapital)
+       
+print("Tuplas Estado na 3 posição")
+print (tuplaEstado[2])
+print("Tuplas Capital na 3 posição")
+print (tuplaCapital[2])
+     
